8ef392886995b6ed5736866b899d385ec791fe0c
Erik S. Carlsten <ecarlste@gmail.com>
2024-06-19 13:30:41 -0600

    added use of nestjs-prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JokesModule } from './jokes/jokes.module';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [ConfigModule.forRoot(), JokesModule],
+  imports: [ConfigModule.forRoot(), JokesModule, PrismaModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/jokes/jokes.module.ts
+++ src/jokes/jokes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JokesService } from './jokes.service';
 import { JokesController } from './jokes.controller';
+import { PrismaService } from 'nestjs-prisma';
 
 @Module({
   controllers: [JokesController],
-  providers: [JokesService],
+  providers: [JokesService, PrismaService],
 })
 export class JokesModule {}
